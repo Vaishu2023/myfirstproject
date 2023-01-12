572da8603df181724fbdf9e19b673ccf22a97394
prash <prash@192.168.0.49>
2023-01-12 16:50:35 -0500

    offer updated for fashion sales

--- src/Training/Hello.java
+++ src/Training/Hello.java
@@ -9,6 +9,7 @@ public class Hello {
 		System.out.println(50*30);
 		System.out.println("Thank you");
 		System.out.println("$150 off on iPhone 14 Pro Max on Saturday night");
+		System.out.println("fashion sales starts this sunday");
 		
 		
 		
