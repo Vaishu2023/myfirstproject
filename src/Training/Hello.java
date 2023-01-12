c9ca0c33757ca873ca3ace321d5a21211269c383
prash <prash@192.168.0.49>
2023-01-12 16:37:21 -0500

    I am uploading project

---
+++ src/Training/Hello.java
@@ -0,0 +1,16 @@
+package Training;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to the world of Devops");
+		System.out.println("This is Vaishali");
+		System.out.println(50*30);
+		System.out.println("Thank you");
+		
+		
+		
+	}
+
+}
